f7e2d8900989e7154b781b563ed4a23fbdf9e1f7
Ben Blamey <blamey.ben@gmail.com>
2018-03-14 11:44:07 +0100

    add some examples

--- auto_coder.py
+++ auto_coder.py
@@ -5,20 +5,27 @@ import itertools
 import random
 import string
 
+
 def parse_man_page(command):
-    global command_man_lines
-    os.system('man {} | col -b > foo.txt'.format(command))
-    with open('foo.txt', 'r') as content_file:
+    os.makedirs('man_cache', exist_ok=True)
+    os.system('man {} | col -b > man_cache/man_{}.txt'.format(command, command))
+    # from: https://unix.stackexchange.com/questions/15855/how-to-dump-a-man-page
+
+    with open('man_cache/man_{}.txt'.format(command), 'r') as content_file:
         content = content_file.read()
     command_man_lines = re.split(r'\n\s*\n', content, flags=re.MULTILINE)
-    command_man_lines = [line for line in command_man_lines if '--' in line]
+    command_man_lines = [line for line in command_man_lines if '-' in line]
+
+    print(command_man_lines)
+
+    # TODO: parse synopsis section also (see tar)
+    # TODO: score for relevence
     return command_man_lines
 
 
-def extract_candidate_args(adjective):
-    global candidate_args
-    relevant_lines = list(itertools.filterfalse(lambda line: adjective not in line, command_man_lines))
-    #print(relevant_lines)
+def extract_candidate_args(command_man_lines, adjective):
+    relevant_lines = list(itertools.filterfalse(lambda line: adjective.lower() not in line.lower(), command_man_lines))
+    # print(relevant_lines)
     candidate_args = []
     for line in relevant_lines:
         # relevant_lines.split(' ').where
@@ -28,31 +35,45 @@ def extract_candidate_args(adjective):
     return candidate_args
 
 
-if __name__ == '__main__':
-    VERB = 'sort'  # TODO: map this to a linux shell command with a search
-    ADJECTIVES = 'reverse'  # TODO: look up in thesaurus
-
-    command_man_lines = parse_man_page(VERB)
-    candidate_args = extract_candidate_args(ADJECTIVES)
-
+def auto_generate(verb, adjective):
+    command_man_lines = parse_man_page(verb)
+    candidate_args = extract_candidate_args(command_man_lines, adjective)
     print(candidate_args)
 
     for i in range(1, 5):
-        args_sample = random.sample(candidate_args, random.randint(1, len(candidate_args)))
+        # TODO: avoid duplicates
+        args_sample = random.sample(candidate_args, random.randint(0, len(candidate_args)))
         random.shuffle(args_sample)
-        #print(args_sample)
+        # print(args_sample)
 
         args = ' '.join(args_sample)
-        print(args)
+        # print(args)
 
-        command = '{} {} <input.txt'.format(VERB, args)
+        print('running...')
+        command = '{} {} <input.txt'.format(verb, args)
         print(command)
 
         exit_value = os.system(command)
-        print(exit_value)
+        print('exit value: {}'.format(exit_value))
 
-    # TODO: extract commands
-    # TODO: extract tokens for search
+        # TODO: cost function - number of arguments
+        # TODO: check return value
+        # TODO: ask user which output is correct
+        # TODO: prefer double-dash
+
+
+if __name__ == '__main__':
+    # TODO: map 1st arg this to a linux shell command with a search
+    # TODO: look up args in thesaurus
+    # TODO: allow soft-matching/partial/word-stem of adjectives
+    # TODO: allow data for args (e.g. for regex)
+
+    auto_generate('sort', 'numeric')
+
+    # auto_generate('tar', 'Extract files')
+
+    # auto_generate('uniq', 'case')
+    #auto_generate('uniq', 'count')
 
 # for line in command_man_lines:
 #     print(line)
--- readme.md
+++ readme.md
@@ -1,5 +1,74 @@
-To get an ASCII man page file, without the annoying backspace/underscore attempts at underlining, and weird sequences to do bolding:
+# Auto-Coder
 
-man ksh | col -b > ksh.txt
+Auto-generate Bash programs for pre-processing given keywords:
 
-https://unix.stackexchange.com/questions/15855/how-to-dump-a-man-page
+# Examples:
+
+Input:
+```
+auto_generate('uniq', 'count')
+```
+
+Output:
+```
+running...
+uniq -c <input.txt
+   1 1
+   1 67
+   1 3
+   1 5
+   1 8
+   1 3
+   1 79
+   1 alice
+   1 charlie
+   1 bob
+exit value: 0
+```
+
+
+
+Input:
+
+```
+auto_generate('sort', 'reverse')
+```
+
+Output:
+```
+running...
+sort -r <input.txt
+charlie
+bob
+alice
+8
+79
+67
+5
+3
+3
+1
+exit value: 0
+```
+
+Input:
+```
+auto_generate('sort', 'numeric')
+```
+
+Output:
+```
+running...
+sort -g --dictionary-order --numeric-sort <input.txt
+alice
+bob
+charlie
+1
+3
+3
+5
+8
+67
+79
+exit value: 0
+```
